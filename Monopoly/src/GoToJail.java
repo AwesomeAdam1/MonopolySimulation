47e5e7ef90d2ecc04a4bb6c6a3c20fe307231bc1
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2025-01-13 17:29:31 -0500

    added to jail

--- Monopoly/src/GoToJail.java
+++ Monopoly/src/GoToJail.java
@@ -1,6 +1,9 @@
 public class GoToJail extends Space {
     public void doAction(Player player) {
-
+        player.inJail = true;
+        player.jailTimeLeft = 3;
+        player.positionIndex = 10;
+        System.out.println("Going to Jail!");
     }
 
     public String toString() {
